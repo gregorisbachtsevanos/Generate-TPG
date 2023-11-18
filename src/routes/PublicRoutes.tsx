6a385a70b3e66bda07cf476ec199c48aaea45ecb
GregorisB <gregorbachts@gmail.com>
2023-11-18 03:28:06 +0200

    changes

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PublicRoutes = ({ accessToken }: { accessToken: string }) => {
 	console.log(accessToken);
-	return !accessToken ? <Outlet /> : <Navigate to="/chat" />;
+	return accessToken ? <Outlet /> : <Navigate to="/chat" />;
 };
 
 export default PublicRoutes;
