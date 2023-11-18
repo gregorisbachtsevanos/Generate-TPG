7754cfa5a7779878cfe3870fd7df4d731ee974eb
GregorisB <gregorbachts@gmail.com>
2023-11-18 03:27:12 +0200

    animation

---
+++ src/components/Animation/Animation.styled.ts
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const StyledAnimationContainer = styled.div`
+	.scan {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.scan .face {
+		position: relative;
+		width: 500px;
+		height: 500px;
+		background: url(" /robot.png");
+		background-size: 500px;
+		animation-fill-mode: forwards;
+		opacity: 1;
+	}
+
+	.scan .face::before {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: url("/man.png");
+		background-size: 500px;
+		animation: animate 4s ease-in-out infinite;
+	}
+
+	.scan .face::after {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 50%;
+		transform: translateX(-50%);
+		width: 90%;
+		height: 8px;
+		background: ${({ theme }) => theme.palette.blue[4]};
+		border-radius: 50%;
+		filter: drop-shadow(0 0 2px ${({ theme }) => theme.palette.blue[4]})
+			drop-shadow(0 0 60px ${({ theme }) => theme.palette.blue[4]});
+		animation: animate-line 4s ease-in-out infinite;
+	}
+
+	@keyframes animate {
+		0%,
+		100% {
+			height: 0%;
+		}
+		50% {
+			height: 100%;
+		}
+	}
+
+	@keyframes animate-line {
+		0%,
+		100% {
+			top: 0%;
+		}
+		50% {
+			top: 100%;
+		}
+	}
+`;
---
+++ src/components/Animation/Animation.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyledAnimationContainer } from "./Animation.styled";
+
+const Animation = () => {
+	return (
+		<StyledAnimationContainer>
+			<div className="scan">
+				<div className="face">
+					<div className="lines"></div>
+				</div>
+			</div>
+		</StyledAnimationContainer>
+	);
+};
+
+export default Animation;
