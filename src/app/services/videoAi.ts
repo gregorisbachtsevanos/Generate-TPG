c82c2f3a385ebce32fe820a160b09b41ffc47462
GregorisB <gregorbachts@gmail.com>
2023-11-21 22:55:40 +0200

    video call

--- src/app/services/videoAi.ts
+++ src/app/services/videoAi.ts
@@ -5,8 +5,8 @@ const replicate = new Replicate({
 	auth: config.replicateKey,
 });
 
-export async function main({ prompt }: { prompt: string }) {
-	const output = await replicate.run(
+export const generateVideo = async ({ prompt }: { prompt: string }) => {
+	const res = await replicate.run(
 		"anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
 		{
 			input: {
@@ -14,5 +14,6 @@ export async function main({ prompt }: { prompt: string }) {
 			},
 		}
 	);
-	console.log(output);
-}
+
+	return res;
+};
