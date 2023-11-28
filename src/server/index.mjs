7937058540cc737f89f5971365b00f8acb48d2c2
GregorisB <gregorbachts@gmail.com>
2023-11-28 23:37:43 +0200

    changes

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -14,6 +14,7 @@ app.use(cors());
 app.get("/video-generator", async (req, res) => {
 	console.log(req)
 	const response = { data: '' }
+
 	// const response = await replicate.run(
 	// 	"stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
 	// 	{
