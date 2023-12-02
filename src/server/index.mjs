b8d36b0cf914bde98d47f9e6c999395c5d17284f
GregorisB <gregorbachts@gmail.com>
2023-12-02 17:55:51 +0200

    changes

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -1,43 +1,40 @@
 import express from "express";
 import cors from 'cors';
 
-// import Replicate from "replicate";
+import Replicate from "replicate";
 
-// const replicate = new Replicate({
-// 	auth: 'r8_LpIfovNTW9GoBwkadO0oDI1czAsXClV3QmkXP',
-// });
+const replicate = new Replicate({
+	auth: 'r8_LpIfovNTW9GoBwkadO0oDI1czAsXClV3QmkXP',
+});
 
 const app = express();
 const port = 3001;
 app.use(cors());
 
 app.get("/video-generator", async (req, res) => {
-	console.log(req)
-	const response = { data: '' }
-	// const response = await replicate.run(
-	// 	"stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
-	// 	{
-	// 		input: {
-	// 			prompt,
-	// 		},
-	// 	}
-	// );
+	const response = await replicate.run(
+		"stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
+		{
+			input: {
+				prompt,
+			},
+		}
+	);
 	res.send({ message: "Image generator", response });
 });
 
 app.get("/image-generator", async (req, res) => {
 	console.log(req.query)
 	const { query } = req
-
-	// const response = await replicate.run(
-	// 	"anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
-	// 	{
-	// 		input: {
-	// 			prompt,
-	// 		},
-	// 	}
-	// );
-	res.send({ message: "Video generator", query });
+	const response = await replicate.run(
+		"anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+		{
+			input: {
+				prompt: query,
+			},
+		}
+	);
+	res.send({ message: "Video generator", response });
 });
 
 app.listen(port, () => {
