640dea6752eb45009716233f01486f60d0eff52a
GregorisB <gregorbachts@gmail.com>
2023-12-04 22:05:37 +0200

    changes

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -11,26 +11,28 @@ const app = express();
 const port = 3001;
 app.use(cors());
 
-app.get("/video-generator", async (req, res) => {
+app.get("/image-generator", async (req, res) => {
+	const { query } = req
+
+	// return console.log(query.prompt)
 	const response = await replicate.run(
 		"stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
 		{
 			input: {
-				prompt,
+				prompt: query.prompt,
 			},
 		}
 	);
 	res.send({ message: "Image generator", response });
 });
 
-app.get("/image-generator", async (req, res) => {
-	console.log(req.query)
+app.get("/video-generator", async (req, res) => {
 	const { query } = req
 	const response = await replicate.run(
 		"anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
 		{
 			input: {
-				prompt: query,
+				prompt: query.prompt,
 			},
 		}
 	);
