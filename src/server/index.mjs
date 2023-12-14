6d6cd4767d79a23ee4d0e06b0a8d6f2443ed4df8
GregorisB <gregorbachts@gmail.com>
2023-12-14 23:01:47 +0200

    clean up

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -14,7 +14,6 @@ app.use(cors());
 app.get("/image-generator", async (req, res) => {
 	const { prompt } = req.query
 
-	// return console.log(query.prompt)
 	const response = await replicate.run(
 		"stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
 		{
